10409351f21636c19be5b2f87d987da2304ad9c2
yves labidoire <yves_labidoire@yahoo.com>
2018-11-20 22:48:37 +0900

    crawler

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,59 +1,48 @@
 class Deal < ApplicationRecord
   has_many :historical_deals
 
-  def self.build_deal(origin, airport, date)
-    quotes = Quote.where(origin: origin, destination: airport)
-    puts quotes.count
-    quotes.each do |depart_leg|
-      (2..14).each do |n|
-        puts n
-        return_leg = Quote.find_by(unique_flight: "#{depart_leg.depart_date + n.days}-#{depart_leg.destination}-#{depart_leg.origin}")
-        if return_leg
-          d = Deal.new(depart_date: depart_leg.depart_date, return_date: return_leg.depart_date, origin: depart_leg.origin, destination: depart_leg.destination, price: (depart_leg.min_price + return_leg.min_price))
-          d.unique_deal = "#{d.depart_date}-#{d.return_date}-#{d.origin}-#{d.destination}"
-          found_deal = Deal.find_by(unique_deal: d.unique_deal)
-          if found_deal.nil?
-           d.save
-         else
-          found_deal.price = d.price
-          found_deal.save
-          HistoricalDeal.create!(deal: found_deal, price: found_deal.price)
+  def self.build_deal(origin, airport)
+    today = Date.today
+    1.upto(60) do |x|
+      depart_leg = depart_leg = Quote.find_by(unique_flight: "#{today + x.days}-#{origin}-#{airport}")
+      # p depart_leg
+      if depart_leg
+        1.upto(15).each do |n|
+          return_leg = Quote.find_by(unique_flight: "#{today + x.days + n.days}-#{airport}-#{origin}")
+          # p return_leg
+          if return_leg
+            d = Deal.new(depart_date: depart_leg.depart_date, return_date: return_leg.depart_date, origin: depart_leg.origin, destination: depart_leg.destination, price: (depart_leg.min_price + return_leg.min_price))
+            d.unique_deal = "#{d.depart_date}-#{d.return_date}-#{d.origin}-#{d.destination}"
+            found_deal = Deal.find_by(unique_deal: d.unique_deal)
 
+            if found_deal.nil?
+              d.save
+            else
+            found_deal.price = d.price
+            found_deal.save
+            HistoricalDeal.create!(deal: found_deal, price: found_deal.price)
+            puts '------------------------------'
+            end
+          end
         end
       end
     end
   end
-end
-
-def self.data_fetch
- DATES.each  do |date|
-   AIRPORTS.each do |airport|
-     puts 'geting quotes'
-     Quote.get_quotes("TYOA-sky", airport, date)
-     puts 'bulding deals'
-     Deal.build_deal("TYOA-sky", airport, date)
-     "end of data_fetch-#{airport}-#{date}"
-     sleep(2)
-   end
- end
-end
-
-def self.test
-       # Quote.get_quotes("TYOA-sky", 'HKG-sky', '2018-12')
-
-       Deal.build_deal("TYOA-sky",'HKG-sky', '2018-12')
-       "end of data_fetch-#{airport}-#{date}"
-       # sleep(2)
 
 
-     end
+  def self.data_fetch
+  origin = "TYOA-sky"
+  airports = ['HKG-sky', 'BKKT-sky', 'HNLA-sky', 'TPET-sky', 'SIN-sky']
+  dates = ['2018-12', '2019-01', '2019-02']
+    airports.each do |airport|
+      dates.each do |date|
+      Quote.get_quotes("TYOA-sky", airport, date)
+      end
+    end
+    airports.each do |airport|
+    build_deal(origin, airport)
+    end
+  end
 
 
-
-
-
-     AIRPORTS = ['HKG-sky', 'BKKT-sky', 'HNLA-sky', 'TPET-sky', 'SIN-sky']
-     DATES = ['2018-12', '2019-01', '2019-02', '2019-03']
-
-# Deal.data_fetch("HKG-sky", "2018-12")
 end
