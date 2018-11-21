2bdf51e0f8808ffc1a13a8ee36214a74c0ed0860
Jeremy Hon <jehon11@gmail.com>
2018-11-21 12:28:04 +0900

    can calc discount

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,27 +2,30 @@ class Deal < ApplicationRecord
   has_many :historical_deals
 
 
-  def self.build_deal(origin, airport)
+  def self.build_deal(origin, destination)
     today = Date.today
     1.upto(60) do |x|
-      depart_leg = depart_leg = Quote.find_by(unique_flight: "#{today + x.days}-#{origin}-#{airport}")
-      # p depart_leg
+      depart_leg = Quote.find_by(unique_flight: "#{today + x.days}-#{origin}-#{destination}")
+      # find depart_leg
       if depart_leg
         1.upto(15).each do |n|
-          return_leg = Quote.find_by(unique_flight: "#{today + x.days + n.days}-#{airport}-#{origin}")
-          # p return_leg
+          return_leg = Quote.find_by(unique_flight: "#{today + x.days + n.days}-#{destination}-#{origin}")
+          # find return_leg and check if exists
           if return_leg
             d = Deal.new(depart_date: depart_leg.depart_date, return_date: return_leg.depart_date, origin: depart_leg.origin, destination: depart_leg.destination, price: (depart_leg.min_price + return_leg.min_price))
             d.unique_deal = "#{d.depart_date}-#{d.return_date}-#{d.origin}-#{d.destination}"
             found_deal = Deal.find_by(unique_deal: d.unique_deal)
-
             if found_deal.nil?
               d.save
+              HistoricalDeal.create!(deal: d, price: d.price)
             else
-            found_deal.price = d.price
-            found_deal.save
-            HistoricalDeal.create!(deal: found_deal, price: found_deal.price)
-            puts '------------------------------'
+              found_deal.price = d.price
+              # checking and inserting discount
+              found_deal.discount = found_deal.calc_discount
+              found_deal.save
+              # -----
+              HistoricalDeal.create!(deal: found_deal, price: found_deal.price)
+              puts '------------------------------'
             end
           end
         end
@@ -30,19 +33,31 @@ class Deal < ApplicationRecord
     end
   end
 
-  def self.data_fetch
-  origin = "TYOA-sky"
-  airports = ['HKG-sky', 'BKKT-sky', 'HNLA-sky', 'TPET-sky', 'SIN-sky']
-  dates = ['2018-12', '2019-01', '2019-02']
-    airports.each do |airport|
+  def calc_discount
+    historical_deals = HistoricalDeal.where(deal_id: self.id).order(created_at: :desc).limit(30)
+    last_30_prices = historical_deals.map { |deal| deal.price }
+    average_price = last_30_prices.inject { |sum, el| sum + el }.to_f / last_30_prices.size
+    discount = average_price - self.price
+    return discount
+  end
+
+  def self.crawl
+    origin = "TYOA-sky"
+    destinations = ['HKG-sky']
+    dates = ['2018-12', '2019-01', '2019-02']
+    # building quotes
+    destinations.each do |destination|
       dates.each do |date|
-      Quote.get_quotes("TYOA-sky", airport, date)
+      Quote.get_quotes("TYOA-sky", destination, date)
       end
     end
-    airports.each do |airport|
-    build_deal(origin, airport)
+    # building deals
+    destinations.each do |destination|
+      build_deal(origin, destination)
     end
   end
 
+  # dates = ['2018-12', '2019-01', '2019-02']
+  # destinations = ['HKG-sky', 'BKKT-sky', 'HNLA-sky', 'TPET-sky', 'SELA-sky']
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_055311) do
+ActiveRecord::Schema.define(version: 2018_11_21_025555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_055311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "unique_deal", default: ""
+    t.integer "discount"
   end
 
   create_table "historical_deals", force: :cascade do |t|
