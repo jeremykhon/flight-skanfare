962bd141b2427d69a16f4fe3a98c23075c28a71f
Jeremy Hon <jehon11@gmail.com>
2018-11-29 12:38:53 +0900

    creating preference sweet alert and login screens

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -4,9 +4,10 @@ class PreferencesController < ApplicationController
     @preference = Preference.new(preference_params)
     @preference.user = current_user
     if @preference.save
+      sleep 2
       redirect_to deals_path
     else
-      redirect_to profile_path
+      redirect_to new_user_session_path
     end
   end
 
