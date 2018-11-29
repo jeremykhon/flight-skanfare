3b6c052d5d3bdca847b69c12d84e9491614f38a1
Sylvain AZZARELLI <sylvainazzarelli@MacBook-Pro-de-Sylvain-2.local>
2018-11-29 12:25:35 +0900

    one picture homepage

--- app/javascript/packs/image_animate.js
+++ app/javascript/packs/image_animate.js
@@ -7,32 +7,36 @@
 
 // console.log('background')
 
-$(function() {
-  var body = $('.fullscreen');
-  const gradient = "linear-gradient(-225deg, rgba(0,101,168,0.3) 0%, rgba(0,36,61,0.4) 50%)"
-  var backgrounds = body.data("photos");
-  var current = backgrounds.length - 1;
+// ------ begining of former function ----------
 
-  function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
+// $(function() {
+//   var body = $('.fullscreen');
+//   const gradient = "linear-gradient(-225deg, rgba(0,101,168,0.3) 0%, rgba(0,36,61,0.4) 50%)"
+//   var backgrounds = body.data("photos");
+//   var current = backgrounds.length - 1;
 
-  function nextBackground() {
-    body.animate({opacity: 0}, 500);
-    setTimeout( function(){
-        body.css(
-          'background-image',
-          `${gradient}, url("${backgrounds[current = ++current % backgrounds.length]}")`);
-        body.animate({opacity: 1}, 500).delay(100);
-      }, 500);
+//   function sleep(milliseconds) {
+//     var start = new Date().getTime();
+//     for (var i = 0; i < 1e7; i++) {
+//       if ((new Date().getTime() - start) > milliseconds){
+//         break;
+//       }
+//     }
+//   }
 
-    setTimeout(nextBackground, 7000);
-  }
+//   function nextBackground() {
+//     body.animate({opacity: 0}, 500);
+//     setTimeout( function(){
+//         body.css(
+//           'background-image',
+//           `${gradient}, url("${backgrounds[current = ++current % backgrounds.length]}")`);
+//         body.animate({opacity: 1}, 500).delay(100);
+//       }, 500);
 
-  nextBackground();
-});
+//     setTimeout(nextBackground, 7000);
+//   }
+
+//   nextBackground();
+// });
+// ------ end of former function ----------
+
